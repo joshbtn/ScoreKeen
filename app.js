09aaa485b1f3aeb3eec3c60d88ca470e2b174fce
Josh Bennett <josh@j-ben.com>
2012-05-31 16:26:20 -0400

    working out a quick interface

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , sass = require('stylus');
 
 var app = module.exports = express.createServer();
 
@@ -16,15 +17,21 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  this.use(require("stylus").middleware({
+      src: __dirname + "/public",
+      compress: true
+  }));
   app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(express.static(__dirname + '/public', { maxAge: 0 }));
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler());
+  app.use(express.static(__dirname + '/public'));
 });
 
 // Routes
