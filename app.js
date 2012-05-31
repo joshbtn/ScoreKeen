214f2de1f762553dad97d1992de64134d522d681
Josh Bennett <josh@j-ben.com>
2012-05-31 17:31:05 +0000

    added heroku env.port

--- app.js
+++ app.js
@@ -31,5 +31,6 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(process.env.C9_PORT||3000);
+app.listen(process.env.PORT || process.env.C9_PORT || 3000);
+
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
