d7c9fe05e3e51154b5d69a30fc517c2a45b45488
Josh Bennett <josh@j-ben.com>
2012-05-31 16:59:17 -0400

    added, add win button

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
+var pg_config = require('pg_config.js')
+	, pg = require('pg');
 
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Foosball' })
+  res.render('index', { title: 'Foosball' });
 };
