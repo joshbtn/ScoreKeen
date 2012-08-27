33fd32046eeda945b7e86af5cf82e6710a299b77
Josh Bennett <josh@j-ben.com>
2012-08-27 15:11:47 -0400

    fixed bar issue height

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var pg = require('pg')
+    ,_ = require('underscore')
     ,pgConfig = require('../pg_config.js');
 
 /*
@@ -18,17 +19,17 @@ exports.index = function(req, res){
     
     pg.connect(connectionString, function(err, client) {
         client.query('SELECT * FROM score', [], function(err, result) {
-            score1 = result.rows[0].value;
-            score2 = result.rows[1].value;
+            score1 = _.find(result.rows, function(res){return res.id == 1}).value;
+            score2 = _.find(result.rows, function(res){return res.id == 2}).value;
             
             res.render('index', { 
                 title: 'Foosball'
                 , score1: score1
                 , score2: score2
                 , bar1Width: Math.min( (score1 / MAX_SCORE) * 100, 100 )
-                , bar1Color: score1 > score2 ? 'progress-success' : 'progress-warning'
+                , bar1Color: score1 >= score2 ? 'progress-success' : 'progress-warning'
                 , bar2Width: Math.min( (score2 / MAX_SCORE) * 100, 100 )
-                , bar2Color: score1 < score2 ? 'progress-success' : 'progress-warning'
+                , bar2Color: score1 <= score2 ? 'progress-success' : 'progress-warning'
             });
         });
     });
