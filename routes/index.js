1e2579514b5a91ddc51424d62615510f99f91a85
Josh Bennett <josh@j-ben.com>
2012-08-27 14:15:19 -0400

    hooked up to pg

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
+var pg = require('pg')
+    ,pgConfig = require('../pg_config.js');
 
 /*
  * GET home page.
  */
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Foosball' });
+    var MAX_SCORE = 10
+        score1 = 0
+        , score2 = 0
+        ,connectionString = 
+            "postgres://" + pgConfig.user + ":" 
+            + pgConfig.password  + "@"  + pgConfig.host 
+            + ":" + pgConfig.port 
+            + "/" + pgConfig.dbname;
+    
+    
+    pg.connect(connectionString, function(err, client) {
+        client.query('SELECT * FROM score', [], function(err, result) {
+            score1 = result.rows[0].value;
+            score2 = result.rows[1].value;
+            
+            res.render('index', { 
+                title: 'Foosball'
+                , score1: score1
+                , score2: score2
+                , bar1Width: Math.min( (score1 / MAX_SCORE)* 100, 10 )
+                , bar1Color: score1 > score2 ? 'progress-success' : 'progress-warning'
+                , bar2Width: Math.min( (score2 / MAX_SCORE)* 100, 10 )
+                , bar2Color: score1 < score2 ? 'progress-success' : 'progress-warning'
+            });
+        });
+    });
 };
